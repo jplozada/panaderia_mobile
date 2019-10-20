7ad405cfc2b5080b1bbb9ab53938bbb3ad6a133d
jplozada <juxnpx@gmail.com>
2019-10-20 08:59:23 -0400

    Crud & BottomNavigationBar

---
+++ lib/ui/views/homeView.dart
@@ -0,0 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:prueba1/core/models/productModel.dart';
+import 'package:prueba1/core/viewmodels/CRUDModel.dart';
+import 'package:prueba1/ui/widgets/productCard.dart';
+import 'package:provider/provider.dart';
+import 'package:prueba1/ui/views/readProduct.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {int _currentIndex = 0;
+  final List<Widget> _children = [
+    Page1(Colors.white),
+    Page2(Colors.deepOrange),
+    Page3(Colors.green)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            title: Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile')
+          )
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    }); 
+  }
+}
+
+//Placeholder
+
+class Page1 extends StatelessWidget {
+  final Color color;
+
+  Page1(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return ReadProduct();
+  }
+}
+
+class Page2 extends StatelessWidget {
+  final Color color;
+
+  Page2(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Pagina 2'),),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  final Color color;
+
+  Page3(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Pagina 3'),),
+    );
+  }
+}
+

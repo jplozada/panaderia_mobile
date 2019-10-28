aabdeef0ca39e25f6792950f180ad15d05d562c8
jplozada <juxnpx@gmail.com>
2019-10-27 21:03:52 -0400

    QR code implemented

--- lib/ui/views/homeVentas.dart
+++ lib/ui/views/homeVentas.dart
@@ -1,8 +1,5 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class HomeVentas extends StatelessWidget {
   @override
@@ -14,9 +11,51 @@ class HomeVentas extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: QrCode(),
         ),
       ),
+    );
+  }
+}
+
+class QrCode extends StatefulWidget {
+  QrCode({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _QrCodeState createState() => _QrCodeState();
+}
+
+class _QrCodeState extends State<QrCode> {
+  String _counter,_value = "";
+  Future _incrementCounter() async{
+    _counter= await FlutterBarcodeScanner.scanBarcode("#004297", "Cancel", true, ScanMode.DEFAULT);
+    setState(() {
+      _value=_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Valor del Scan:',
+            ),
+            Text(
+              _value,
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.settings_overscan),
+      ), 
     );
   }
 }
